30f8d30e49fd2deca17b019e7010f3e99bbb2294
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-04-23 14:32:27 -0400

    Updated from Brython Server: 4/23/2018 2:32:26 PM

--- jarProblem.py
+++ jarProblem.py
@@ -1,1 +1,7 @@
+#Clay Kynor & Greg Phillips
+#4.23.18
+#jarProblem.py
 
+N = int(input("Please enter a total amount of marbles (N value) between 8 and 20: "))
+W = int(input("Please enter the money made when correct guess (W value) between 3 and 15: "))
+D = W*2
