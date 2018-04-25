8f478ea6e691be2f13a6c9317f5cff7eba2d5ff7
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-04-25 16:35:22 -0400

    Updated from Brython Server: 4/25/2018 4:35:21 PM

--- jarProblem.py
+++ jarProblem.py
@@ -61,26 +61,23 @@ from random import randint
 
 N = 10
 W = 8
-RUNS = 1
+RUNS = 1000
 
-total = 10
+total = 0
 
 for i in range(0,RUNS):
     green = randint(0,N)
     red = N-green
     Pr = red/N
     Pg = green/N
-    p1 = Pr**2
-    p2 = Pg**2
+    p1 = Pr*Pr*Pr
+    p2 = Pg*Pg*Pg
     p3 = Pg*Pr*Pr*Pr
     p4 = Pg*Pr*Pg*Pg
     p5 = Pr*Pg*Pr*Pr
     p6 = Pr*Pg*Pg*Pg
+    total += p1+p2+p3+p4+p5+p6
     
-    
-    
-    print(p1+p2+p3+p4+p5+p6)
-
 print(total/RUNS)
 
 
