062847a06a8704d7b962067936f11b44fafce83a
Clay190 <31509660+Clay190@users.noreply.github.com>
2018-04-26 20:34:05 -0400

    Updated from Brython Server: 4/26/2018 8:34:04 PM

--- jarProblem.py
+++ jarProblem.py
@@ -31,11 +31,19 @@ for i in range(0,RUNS):
     p6 = (red/N)*((green)/(N-1))*((red-1)/(N-2))*((red-2)/(N-3))
     total5 += p1+p2+p3+p4+p5+p6
 
-D = (total1/RUNS)*W
+D1 = (total1/RUNS)*W
 D2 = (total2/RUNS)*W
+P = (total3/RUNS)*W-D1
+R = ((total4/RUNS)*W)-D1
+S = ((total5/RUNS)*W)-D1
 
-print("Senerio 1", total1/RUNS)
-print("Senerio 2", total2/RUNS)
-print("Senerio 3", total3/RUNS)
-print("Senerio 4", total4/RUNS)
-print("Senerio 5", total5/RUNS)
+print("Senerio 1:", total1/RUNS)
+print("D for senerio 1 is", D1)
+print("Senerio 2:", total2/RUNS)
+print("D for senerio 2 is", D2)
+print("Senerio 3:", total3/RUNS)
+print("P for senerio 3 is", S)
+print("Senerio 4:", total4/RUNS)
+print("R for senerio 4 is", R)
+print("Senerio 5:", total5/RUNS)
+print("S for senerio 5 is", S)
